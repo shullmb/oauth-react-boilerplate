28509c0e57378bbfb1d35b2590efb4b64756d7a1
Michael Shull <shullmb@gmail.com>
2019-08-13 14:22:49 -0700

    outh working -- react app started

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import openNewAuthWindow from "./helpers/openWindow";
+
+import "./App.css";
+
+export interface IUser {
+	_id?: string;
+	githubId: number;
+	accessToken?: string;
+}
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+	const [user, setUser] = useState<IUser>({} as IUser);
+	const [repose, setRepos] = useState([]);
+
+	function handleLogin(e: React.MouseEvent): void {
+		e.preventDefault();
+		let message: Promise<IUser> = openNewAuthWindow("/auth/github");
+		message.then(res => setUser(res));
+	}
+
+	useEffect(() => {
+		console.log("useEffect fetching data");
+		if (Object.keys(user).length) {
+			axios.get(`/api/${user.githubId}/repos`).then(res => {
+				setRepos(res.data);
+			});
+		} else {
+		}
+	}, [user]);
+
+	return (
+		<div className="App">
+			<a href="/auth/github">Login with Github</a>
+		</div>
+	);
+};
 
 export default App;
---
+++ client/src/helpers/openWindow.ts
@@ -0,0 +1,5 @@
+import { IUser } from "../App";
+
+export default function openNewAuthWindow(uri: string): Promise<IUser> {
+  return new Promise<IUser>
+}

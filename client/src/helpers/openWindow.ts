2464171e6acd3851fb70687d2a8b3659aacb529b
Michael Shull <shullmb@gmail.com>
2019-08-13 14:41:42 -0700

    auth promise complete

--- client/src/helpers/openWindow.ts
+++ client/src/helpers/openWindow.ts
@@ -1,5 +1,37 @@
 import { IUser } from "../App";
 
 export default function openNewAuthWindow(uri: string): Promise<IUser> {
-  return new Promise<IUser>
+	// open a window
+	const authWindow = window.open(uri, "_blank") as Window;
+
+	// listen for messages from authWindow
+	const authPromise: Promise<IUser> = new Promise((resolve, reject) => {
+		window.addEventListener(
+			"message",
+			msg => {
+				if (
+					!msg.origin.includes(
+						`${window.location.protocol}//${window.location.host}`
+					)
+				) {
+					authWindow.close();
+					reject("NotAllowed");
+				}
+				if (msg.data.payload) {
+					try {
+						resolve(JSON.parse(msg.data.payload));
+					} catch (err) {
+						resolve(msg.data.payload);
+					} finally {
+						authWindow.close();
+					}
+				} else {
+					authWindow.close();
+					reject("Unauthorized");
+				}
+			},
+			false
+		);
+	});
+	return authPromise;
 }
